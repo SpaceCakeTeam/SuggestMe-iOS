cf6b88f616b5570fae76562bf9d227e6682abca1
matcmd <matcmd90@gmail.com>
2015-05-09 11:37:42 +0200

    Initial Commit

---
+++ SuggestMe-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SuggestMe-iOS
+//
+//  Created by Mattia Uggè on 09/05/15.
+//  Copyright (c) 2015 Mattia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SuggestMe-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SuggestMe-iOS
+//
+//  Created by Mattia Uggè on 09/05/15.
+//  Copyright (c) 2015 Mattia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
