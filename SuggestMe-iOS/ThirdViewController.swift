fef8a09fd2477966d730c41755bdc41661d3d790
matcmd <matcmd90@gmail.com>
2015-05-10 14:28:58 +0200

    Utility, CommunicationHandler and Main Storyboard updated!

---
+++ SuggestMe-iOS/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  SuggestMe-iOS
+//
+//  Created by Mattia Uggè on 10/05/15.
+//  Copyright (c) 2015 Mattia. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
--- SuggestMe-iOS/Utility.swift
+++ SuggestMe-iOS/Utility.swift
@@ -22,7 +22,22 @@ class Utility {
         return Static.instance!
     }
     
-    func hasConnectivity() -> Bool {
-        return true
+    func isConnectedToNetwork() -> Bool {
+        var Status: Bool = false
+        let url = NSURL(string: "http://google.com/")
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "HEAD"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData
+        request.timeoutInterval = 10.0
+        
+        var response: NSURLResponse?
+        var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: nil) as NSData?
+        
+        if let httpResponse = response as? NSHTTPURLResponse {
+            if httpResponse.statusCode == 200 {
+                Status = true
+            }
+        }
+        return Status
     }
 }
