7b1e72450a2e104cc668f644c801907cfa3de43b
matcmd <matcmd90@gmail.com>
2015-09-26 12:08:27 +0200

    Aggiunto testo About.

--- SuggestMe-iOS/AboutViewController.swift
+++ SuggestMe-iOS/AboutViewController.swift
@@ -32,7 +32,15 @@ class AboutViewController: UIViewController {
         let backgroundView = UIImageView(image: UIImage(named: "AboutBackground-\(Int(helpers.screenHeight))h"))
         self.view.addSubview(backgroundView)
 		
-		//TODO
+		let description = ALabel(frame: CGRectMake(0, helpers.screenHeight-tabBarHeight-navigationBarHeight-240, helpers.screenWidth, 190))
+        if helpers.screenHeight == 480 {
+            description.frame.origin.y += 30
+        }
+        description.font = UIFont(name: "Freestyle Script", size: 23)
+        description.text = "SuggestMe è come un amico sincero, disponibile e sempre pronto ad aiutarti dandoti consigli in ogni  ambito della tua vita. Ti daremo consigli su tutto ciò che vorrai, senza secondi fini e nel massimo della privacy, troppo spesso violata quando ci si rivolge agli amici sbagliati."
+        description.numberOfLines = 6
+        description.textColor = UIColor.whiteColor()
+        self.view.addSubview(description)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -43,5 +51,11 @@ class AboutViewController: UIViewController {
     //MARK: UIButton Actions
     func login(sender: AnyObject) {
         self.performSegueWithIdentifier("presentLoginViewController", sender: self)
+    }
+}
+
+class ALabel: UILabel {
+    override func drawTextInRect(rect: CGRect) {
+        super.drawTextInRect(UIEdgeInsetsInsetRect(rect, UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)))
     }
 }
--- SuggestMe-iOS/AppDelegate.swift
+++ SuggestMe-iOS/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         storyboard = UIStoryboard(name: "Main", bundle: nil)
+        helpers.printFonts()
         return true
     }
 	
